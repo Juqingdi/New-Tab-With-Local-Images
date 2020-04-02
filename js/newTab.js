96cd1227c549a52da793391c3ba466127e62ccec
Juqingdi <grain2011@sina.com>
2020-04-02 18:26:48 +0800

    topSites

---
+++ js/newTab.js
@@ -0,0 +1,30 @@
+window.onload = ()=>{
+	chrome.topSites.get((topSites)=>{
+		console.log(topSites);
+		let mvTiles = document.getElementById('mv-tiles');
+		let template = document.getElementById('productTopSite');
+		let gridTileContainer = template.content.querySelectorAll('.grid-tile-container')[0];
+		let mdTile = template.content.querySelectorAll('.md-tile')[0];
+		let mdIcon = template.content.querySelectorAll('.md-icon')[0];
+		let mdTitleSpan = template.content.querySelectorAll('.md-title span')[0];
+		let mdEditMenu = template.content.querySelectorAll('button.md-edit-menu')[0];
+
+		let topSite;
+		for (let i = topSites.length - 1; i >= 0; i--) {
+			topSite = topSites[i];
+			gridTileContainer.rid = gridTileContainer.index = i;
+			let x = 112 * (i % 5);
+			let y = 128 * parseInt(i / 5);
+			gridTileContainer.style.transform = `translate(${x}px, ${y}px)`;
+			mdTile.href = topSite.url;
+			mdTile.ariaLabel = mdTile.title = topSite.title;
+			mdTile.dataRid = i + 1;
+			mdTile.dataPos = i;
+			mdIcon.src = `chrome://favicon/size/16@2x/${topSite.url}`;
+			mdTitleSpan.innerText = topSite.title;
+			mdEditMenu.ariaLabel = `修改快捷方式 ${topSite.title}`;
+			let clone = document.importNode(template.content, true);
+			mvTiles.appendChild(clone);
+		}
+	});
+}
